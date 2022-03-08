79c89491d64311c87f9089ac8201c9f0ea3f2402
Hannes Hoffmann <hanneshoffmann@t-online.de>
2022-03-08 05:18:19 +0100

    ADD basic order display

---
+++ lib/view/fragments/order_fragment.dart
@@ -0,0 +1,61 @@
+import 'package:beerbox/model/order.dart';
+import 'package:flutter/material.dart';
+
+class OrderFragment extends StatelessWidget {
+
+  final int _tableNumber;
+  final Order _order;
+
+  const OrderFragment(this._tableNumber, this._order, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      color: Colors.blueGrey.shade800,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Flexible(
+            child: Text(_order.id.toString()),
+            flex: 1,
+          ),
+          Flexible(
+            child: Text(_tableNumber.toString()),
+            flex: 1,
+          ),
+          Flexible(
+            child: Text(_order.orderedItems.length.toString()),
+            flex: 1,
+          ),
+          Flexible(
+            child: Row(
+              children: [
+                Text(_order.getFullCosts().toString()),
+                Text(
+                  'NOK',
+                  style: TextStyle(fontSize: 8),
+                ),
+              ],
+            ),
+            flex: 3,
+          ),
+          Flexible(
+            child: Text(_order.timestamp.toString()),
+            flex: 3,
+          ),
+          Flexible(
+            child: IconButton(
+              onPressed: () => print('printed'),
+              icon: Icon(
+                Icons.print,
+                color: Colors.grey[100],
+              ),
+            ),
+            flex: 1,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/orders_screen.dart
@@ -0,0 +1,120 @@
+import 'package:beerbox/model/customer.dart';
+import 'package:beerbox/model/item.dart';
+import 'package:beerbox/model/item_type.dart';
+import 'package:beerbox/model/order.dart';
+import 'package:beerbox/model/ordered_item.dart';
+import 'package:beerbox/model/table.dart';
+import 'package:beerbox/view/fragments/order_fragment.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+
+class OrdersScreen extends StatefulWidget {
+  const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  List<CustomerTable> tables = [CustomerTable(4, [Order(1, 12142214, Customer("Olaf"), [OrderedItem(2, Item("Pils", 7.2, ItemType.beer))])])];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Orders',
+        ),
+      ),
+      body: DefaultTextStyle(
+        style: TextStyle(color: Colors.grey[100]),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+          color: Colors.grey[850],
+          child: Column(
+            children: [
+              Container(
+                child: DefaultTextStyle(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Flexible(
+                        child: Text('ID'),
+                        flex: 1,
+                      ),
+                      Flexible(
+                        child: Text('TABLE'),
+                        flex: 1,
+                      ),
+                      Flexible(
+                        child: Text('ITEMS'),
+                        flex: 1,
+                      ),
+                      Flexible(
+                        child: Text('COSTS'),
+                        flex: 3,
+                      ),
+                      Flexible(
+                        child: Text('TIMESTAMP'),
+                        flex: 3,
+                      ),
+                      Flexible(
+                        child: Text(''),
+                        flex: 1,
+                      ),
+                    ],
+                  ),
+                  style: TextStyle(
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ),
+              Column(
+                  children: tables.first.orders.map((order) => OrderFragment(5, order)).toList(),
+              ),
+            ],
+          ),
+          // child: Table(
+          //   defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          //   children: [
+          //     TableRow(
+          //       children: [
+          //         TableCell(child: Text('ID')),
+          //         TableCell(child: Text('TABLE')),
+          //         TableCell(child: Text('ITEMS')),
+          //         TableCell(child: Text('COSTS')),
+          //         TableCell(child: Text('TIMESTAMP')),
+          //         TableCell(child: Text('')),
+          //       ],
+          //     ),
+          //     TableRow(
+          //       children: [
+          //         TableCell(child: Text('1')),
+          //         TableCell(child: Text('2')),
+          //         TableCell(child: Text('3')),
+          //         TableCell(child: Text('4.0')),
+          //         TableCell(child: Text('12353')),
+          //         TableCell(
+          //           child: IconButton(
+          //             onPressed: () {
+          //               print('printed');
+          //             },
+          //             icon: Icon(
+          //               Icons.print,
+          //               color: Colors.grey[100],
+          //             ),
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //   ],
+          // ),
+        ),
+      ),
+    );
+  }
+}
