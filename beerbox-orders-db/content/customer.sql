823deaaef3b360b34bf3102841d693e728807a95
Hannes Hoffmann <hanneshoffmann@t-online.de>
2022-04-24 20:54:29 +0200

    ADD more example customers

--- beerbox-orders-db/content/customer.sql
+++ beerbox-orders-db/content/customer.sql
@@ -7,10 +7,18 @@ INSERT INTO "customer"(firstname, familyname) VALUES ('Sindre', 'Discusdude');
 INSERT INTO "customer"(firstname, familyname) VALUES ('Leon', 'Stegosaurus');
 INSERT INTO "customer"(firstname, familyname) VALUES ('Thomas', 'Snoozedaddy');
 INSERT INTO "customer"(firstname, familyname) VALUES ('Hannes', 'Hotzenplotz');
+INSERT INTO "customer"(firstname, familyname) VALUES ('A', 'a');
+INSERT INTO "customer"(firstname, familyname) VALUES ('B', 'b');
+INSERT INTO "customer"(firstname, familyname) VALUES ('C', 'c');
+INSERT INTO "customer"(firstname, familyname) VALUES ('D', 'd');
+INSERT INTO "customer"(firstname, familyname) VALUES ('E', 'e');
+INSERT INTO "customer"(firstname, familyname) VALUES ('F', 'f');
+INSERT INTO "customer"(firstname, familyname) VALUES ('G', 'g');
+INSERT INTO "customer"(firstname, familyname) VALUES ('H', 'h');
 
-UPDATE customer
+UPDATE "customer"
 SET firstname = 'Sindre', familyname = 'Discusdude'
 WHERE firstname = 'Sindre';
 
-DELETE from customer
+DELETE from "customer"
 WHERE _id = -1;
