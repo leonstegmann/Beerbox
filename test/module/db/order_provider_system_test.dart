7540881efbdb11a8012c076bd29e44d42d61cf71
Hannes Hoffmann <hanneshoffmann@t-online.de>
2022-04-25 16:48:01 +0200

    ADD order_provider_system_test.dart

---
+++ test/module/db/order_provider_system_test.dart
@@ -0,0 +1,19 @@
+import 'package:beerbox/controll/customer_provider.dart';
+import 'package:beerbox/controll/order_provider.dart';
+import 'package:beerbox/model/customer.dart';
+import 'package:beerbox/model/order.dart';
+import 'package:beerbox/model/table.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:postgres/postgres.dart';
+
+void main() {
+  group('customer provider', () {
+
+    final OrderProvider op = OrderProvider();
+
+    test('getOrdersPerTable()', () async {
+      Map<CustomerTable, List<Order>> ordersPerTable = await op.getOrdersPerTable();
+      ordersPerTable.forEach((key, value) => print("$key => $value"));
+    });
+  });
+}
