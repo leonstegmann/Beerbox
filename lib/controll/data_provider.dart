1261b8fae1f12e2b25647e73916abc5b4b3fc680
Hannes Hoffmann <hanneshoffmann@t-online.de>
2022-05-13 14:58:26 +0200

    ADD doc to DataProvider

--- lib/controll/data_provider.dart
+++ lib/controll/data_provider.dart
@@ -1,5 +1,9 @@
 import 'package:beerbox/controll/db_crud.dart';
 
+/// An instance to hold a [DbCrud] and provide easy to use methods to interact with it.
+///
+/// This [T] is a Generic to represent the [DbObject] specializing the extending variations of this DataProvider.
+/// The [tableName] has to match with a table in the connected database.
 abstract class DataProvider<T> {
 
   final DbCrud dbCrud = DbCrud.instance;
@@ -7,9 +11,18 @@ abstract class DataProvider<T> {
 
   DataProvider(this.tableName);
 
+  /// Writes a defined [DbObject] to the connected database and returns the created object.
   Future<T> create(T dbObject);
+
+  /// Searches for the specialized [DbObject] in the equivalent database table and returns the entry with the given [id].
   Future<T> read(int id);
+
+  /// Returns all entries from the database table of the specialized [DbObject].
   Future<List<T>> readAll();
+
+  /// Adjusts the database entry with the same id as the [DbObject.id].
   Future<T> update(T dbObject);
+
+  /// Removes the database entry with the given [id] from the specialized [table].
   Future<T> delete(int id);
 }
