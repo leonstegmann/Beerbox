d67896a1b00b3878ff6306c4b56c4b6f511250be
Hannes Hoffmann <hanneshoffmann@t-online.de>
2022-05-13 16:33:52 +0200

    UPDATE doc for control units

--- lib/controll/data_provider.dart
+++ lib/controll/data_provider.dart
@@ -5,6 +5,8 @@ import 'package:beerbox/controll/db_crud.dart';
 /// This [T] is a Generic to represent the [DbObject] specializing the extending variations of this DataProvider.
 /// The [tableName] has to match with a table in the connected database.
 ///
+/// The specialized DataProvider should use [T.fromJson] to extract the objects from the mapped sql-responses.
+///
 /// ## Usage
 /// Use the extending specializations of this class by instantiating them where needed.
 /// The instances will not claim too many resources as the [DbCrud] is implemented as singleton.
--- lib/controll/db_crud.dart
+++ lib/controll/db_crud.dart
@@ -9,6 +9,9 @@ import 'package:postgres/postgres.dart';
 /// All defined operations return a mapped database response used to reconstruct the found [DbObject]'s.
 /// Look into the database tests like [postgres_test.dart] or defined [DataProvider] for better understanding.
 ///
+/// The methods often use [DbObject.toJsonMap] to extract the [DbObject] variables and their content
+/// to a format that is usable for the sql-requests.
+///
 /// ## Usage
 /// It is recommended to never directly use this [DbCrud] outside of a [DataProvider].
 class DbCrud {
@@ -39,6 +42,12 @@ class DbCrud {
 
   get isClosed => _connection.isClosed;
 
+  /// Saves a provided [DbObject] to the defined [table] of the connected database.
+  ///
+  /// It saves [DbObject] to the database by ignoring the [DbObject.id]
+  /// and using all other values to create a new entry in the [table].
+  /// The [id] gets auto-generated.
+  /// The new [DbObject] gets returned in the response.
   Future<List<Map<String, Map<String, dynamic>>>> create(String table, DbObject dbObject) async {
     Map<String, dynamic> jsonMap = dbObject.toJsonMap();
     jsonMap.remove("_id");
@@ -50,14 +59,17 @@ class DbCrud {
         "INSERT INTO $table$variables VALUES $values RETURNING *");
   }
 
+  /// Searches for a specified entry in a database [table] by [id].
   Future<List<Map<String, Map<String, dynamic>>>> read(String table, int id) async {
     return (await connection).mappedResultsQuery("SELECT * FROM $table WHERE _id = $id");
   }
 
+  /// Returns all entries in the defined [table].
   Future<List<Map<String, Map<String, dynamic>>>> readAll(String table) async {
     return (await connection).mappedResultsQuery("SELECT * FROM $table");
   }
 
+  /// Adjusts a [table] entry found for the [DbObject.id].
   Future<List<Map<String, Map<String, dynamic>>>> update(String table, DbObject dbObject) async {
     return (await connection).mappedResultsQuery(
         "UPDATE $table"
@@ -66,6 +78,7 @@ class DbCrud {
             "RETURNING *");
   }
 
+  /// Removes the [table] entry with the specified [id].
   Future<List<Map<String, Map<String, dynamic>>>> delete(String table, int id) async {
     return (await connection).mappedResultsQuery("DELETE from $table WHERE _id = $id RETURNING *");
   }
