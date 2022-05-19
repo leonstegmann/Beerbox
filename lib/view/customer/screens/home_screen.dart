f10110c4114b58010517d47b7ec8ed4c219cc2eb
Leon <lion30021@gmail.com>
2022-05-19 19:33:41 +0200

    UPDATE button style on HomeScreen for even button style on Customer side

--- lib/view/customer/screens/home_screen.dart
+++ lib/view/customer/screens/home_screen.dart
@@ -16,35 +16,26 @@ class HomeScreen extends StatelessWidget {
         alignment: Alignment.center,
         child: Column(
           children: [
-            Flexible(
-              flex: 1,
-              child: Text(
-                'Welcome to Bluebox!',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                    child: const Text('Order'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Menu()),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              ),
+            Text(
+              'Welcome to Bluebox!',
+              style: Theme.of(context).textTheme.headline5,
             ),
           ],
         ),
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: FloatingActionButton.extended(
+          label: const Text('Order'),
+          onPressed: () =>
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Menu()),
+              ),
+          backgroundColor: Theme.of(context).hintColor,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
